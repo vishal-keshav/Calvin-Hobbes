e0592329f595071043271da2dee240963dd66940
Vishal Keshav <Vishal Keshav>
2017-01-03 22:55:41 +0530

    Sample for testing

---
+++ SimpleMobilePlaceholder/Assets/Scripts/SpinningCube.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpinningCube : MonoBehaviour 
+{
+	public float m_Speed = 20f;
+
+	private Vector3 m_RotationDirection = Vector3.up;
+
+	public void ToggleRotationDirection()
+	{
+		Debug.Log ("Toggling rotation new direction");
+
+		if (m_RotationDirection == Vector3.up) 
+		{
+			m_RotationDirection = Vector3.down;
+            m_Speed = m_Speed+20;
+		}
+		else 
+		{
+			m_RotationDirection = Vector3.up;
+            m_Speed = m_Speed + 20;
+        }
+	}
+
+	void Update() 
+	{
+		transform.Rotate(m_RotationDirection * Time.deltaTime * m_Speed);
+	}
+}
