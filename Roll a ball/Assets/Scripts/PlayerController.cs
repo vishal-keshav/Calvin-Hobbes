eb430136671fc6598bc0afa720d93fec3cf6aa65
bulletcross <bulletcross@gmail.com>
2017-01-15 22:53:00 +0530

    Added Joystick

--- Roll a ball/Assets/Scripts/PlayerController.cs
+++ Roll a ball/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,9 @@ public class PlayerController : MonoBehaviour {
     private Rigidbody rb;
     private int points;
     public Text information;
+
+    public Vir_jystick joystick;
+
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -19,11 +22,16 @@ public class PlayerController : MonoBehaviour {
         ///float moveHorizontal = Input.GetAxis("Horizontal");
         ///float moveVertical = Input.GetAxis("Vertical");
         ///Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-		
+
+        ///Vector3 movement = Vector3.zero;
+        ///movement.x = Input.acceleration.x;
+        ///movement.z = Input.acceleration.y;
+
         Vector3 movement = Vector3.zero;
-		movement.x = Input.acceleration.x;
-		movement.z = Input.acceleration.y;
-		if (movement.sqrMagnitude > 1)
+        movement.x = joystick.JHorizontal();
+        movement.z = joystick.JVertical();
+
+        if (movement.sqrMagnitude > 1)
             movement.Normalize();
         rb.AddForce(movement*playerspeed);
     }
---
+++ Roll a ball/Assets/Scripts/Vir_jystick.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System;
+
+public class Vir_jystick : MonoBehaviour, IDragHandler, IPointerUpHandler, IPointerDownHandler {
+
+    private Image bgImg;
+    private Image joystickImg;
+    private Vector3 inputVector;
+
+    private void Start()
+    {
+        bgImg = GetComponent<Image>();
+        joystickImg = transform.GetChild(0).GetComponent<Image>();
+    }
+    //Implementing handlers
+    public virtual void OnDrag(PointerEventData eventData)
+    {
+        Vector2 pos;
+        if(RectTransformUtility.ScreenPointToLocalPointInRectangle(bgImg.rectTransform,eventData.position, eventData.pressEventCamera,out pos))
+        {
+            pos.x = (pos.x / bgImg.rectTransform.sizeDelta.x);
+            pos.y = (pos.y / bgImg.rectTransform.sizeDelta.y);
+
+            inputVector = new Vector3(pos.x * 2 + 1, 0, pos.y * 2 - 1);
+            ///Debug.Log(inputVector);
+            inputVector = (inputVector.magnitude > 1.0f) ? inputVector.normalized : inputVector;
+
+            joystickImg.rectTransform.anchoredPosition = new Vector3(inputVector.x * (bgImg.rectTransform.sizeDelta.x / 3), inputVector.z * (bgImg.rectTransform.sizeDelta.y / 3));
+        }
+
+    }
+    public virtual void OnPointerDown(PointerEventData eventData)
+    {
+        OnDrag(eventData);
+    }
+
+    public virtual void OnPointerUp(PointerEventData eventData)
+    {
+        inputVector = Vector3.zero;
+        joystickImg.rectTransform.anchoredPosition = Vector3.zero;
+    }
+    ///Declare public funtion to give movement interface to ball script
+    public float JHorizontal()
+    {
+        return inputVector.x;
+    }
+    public float JVertical()
+    {
+        return inputVector.z;
+    }
+}
