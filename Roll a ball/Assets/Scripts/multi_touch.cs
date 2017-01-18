429db7a16d1434399805264fef1e04026d97d0d9
bulletcross <bulletcross@gmail.com>
2017-01-18 23:55:20 +0530

    GUI texture not working

--- Roll a ball/Assets/Scripts/multi_touch.cs
+++ Roll a ball/Assets/Scripts/multi_touch.cs
@@ -3,36 +3,41 @@ using System.Collections;
 
 public class multi_touch : MonoBehaviour {
 
-	
-	// Update is called once per frame
-	public void Check_touches  () {
+    //public GUITexture test;
+    // Update is called once per frame
+    public void Check_touches  () {
 	    if(Input.touches.Length <= 0)
         {
 
         }
         else
         {
+            
             for(int i = 0; i < Input.touchCount; i++)
             {
-                if(Input.GetTouch(i).phase == TouchPhase.Began)
+                if (this.GetComponent<GUITexture>() != null && (this.GetComponent<GUITexture>().HitTest(Input.GetTouch(i).position)))
                 {
-                    ///Debug.Log("Button has been touched at" + Input.GetTouch(i).position);
-                    this.SendMessage("OnTouchBeganAnywhere");
-                }
-                if (Input.GetTouch(i).phase == TouchPhase.Ended)
-                {
-                    ///Debug.Log("Button has been ended at" + Input.GetTouch(i).position);
-                    this.SendMessage("OnTouchEndedAnywhere");
-                }
-                if (Input.GetTouch(i).phase == TouchPhase.Moved)
-                {
-                    ///Debug.Log("Button has been ended at" + Input.GetTouch(i).position);
-                    this.SendMessage("OnTouchMovedAnywhere");
-                }
-                if (Input.GetTouch(i).phase == TouchPhase.Stationary)
-                {
-                    ///Debug.Log("Button has been ended at" + Input.GetTouch(i).position);
-                    this.SendMessage("OnTouchStationaryAnywhere");
+                    if (Input.GetTouch(i).phase == TouchPhase.Began)
+                    {
+                        ///Debug.Log("Button has been touched at" + Input.GetTouch(i).position);
+                        this.SendMessage("OnTouchBeganAnywhere");
+                    }
+                    if (Input.GetTouch(i).phase == TouchPhase.Ended)
+                    {
+                        ///Debug.Log("Button has been ended at" + Input.GetTouch(i).position);
+                        this.SendMessage("OnTouchEndedAnywhere");
+                    }
+                    if (Input.GetTouch(i).phase == TouchPhase.Moved)
+                    {
+                        ///Debug.Log("Button has been ended at" + Input.GetTouch(i).position);
+                        this.SendMessage("OnTouchMovedAnywhere");
+                    }
+                    if (Input.GetTouch(i).phase == TouchPhase.Stationary)
+                    {
+                        ///Debug.Log("Button has been ended at" + Input.GetTouch(i).position);
+                        this.SendMessage("OnTouchStationaryAnywhere");
+                    }
+
                 }
 
             }
--- Roll a ball/Assets/Scripts/touch_testing.cs
+++ Roll a ball/Assets/Scripts/touch_testing.cs
@@ -4,9 +4,11 @@ using System.Collections;
 public class touch_testing : multi_touch
 {
 
-    public float rotateSpeed = 20.0f;
+    public float rotateSpeed = 100.0f;
     private float pitch = 0.0f;
     private float yaw = 0.0f;
+
+    public Transform myplayer;
 
     void Update()
     {
@@ -19,6 +21,6 @@ public class touch_testing : multi_touch
         yaw += Input.GetTouch(0).deltaPosition.x * rotateSpeed * Time.deltaTime;
 
         pitch = Mathf.Clamp(pitch, -60, 60);
-        this.transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+        myplayer.transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
     }
 }
